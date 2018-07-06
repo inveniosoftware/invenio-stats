7698b8da32820aa4d0d3cb2f7f872471065e8611
Alexander Ioannidis <a.ioannidis@cern.ch>
2018-07-06 11:45:08 +0200

    aggregations: add ".delete" and ".list_bookmarks"

--- invenio_stats/aggregations.py
+++ invenio_stats/aggregations.py
@@ -299,3 +299,60 @@ class StatAggregator(object):
             )
             if lower_limit > upper_limit:
                 break
+
+    def list_bookmarks(self, start_date=None, end_date=None, limit=None):
+        """List the aggregation's bookmarks."""
+        query = Search(
+            using=self.client,
+            index=self.aggregation_alias,
+            doc_type='{0}-bookmark'.format(self.event)
+        ).sort({'date': {'order': 'desc'}})
+
+        range_args = {}
+        if start_date:
+            range_args['gte'] = start_date.replace(microsecond=0).isoformat()
+        if end_date:
+            range_args['lte'] = end_date.replace(microsecond=0).isoformat()
+        if range_args:
+            query = query.filter('range', date=range_args)
+
+        return query[0:limit].execute() if limit else query.scan()
+
+    def delete(self, start_date=None, end_date=None):
+        """Delete aggregation documents."""
+        aggs_query = Search(
+            using=self.client,
+            index=self.aggregation_alias,
+            doc_type='{0}-{1}-aggregation'.format(
+                self.event, self.aggregation_interval)
+        ).extra(_source=False)
+
+        range_args = {}
+        if start_date:
+            range_args['gte'] = start_date.replace(microsecond=0).isoformat()
+        if end_date:
+            range_args['lte'] = end_date.replace(microsecond=0).isoformat()
+        if range_args:
+            aggs_query = aggs_query.filter('range', timestamp=range_args)
+
+        bookmarks_query = Search(
+            using=self.client,
+            index=self.aggregation_alias,
+            doc_type='{0}-bookmark'.format(self.event)
+        ).sort({'date': {'order': 'desc'}})
+
+        if range_args:
+            bookmarks_query = bookmarks_query.filter('range', date=range_args)
+
+        def _delete_actions():
+            for query in (aggs_query, bookmarks_query):
+                affected_indices = set()
+                for doc in query.scan():
+                    affected_indices.add(doc.meta.index)
+                    yield dict(_index=doc.meta.index,
+                               _op_type='delete',
+                               _id=doc.meta.id,
+                               _type=doc.meta.doc_type)
+                current_search_client.indices.flush(
+                    index=','.join(affected_indices), wait_if_ongoing=True)
+        bulk(self.client, _delete_actions(), refresh=True)
