d877ae5462084abb4a28a20f1ebb3d636769c1bc
Alexander Ioannidis <a.ioannidis@cern.ch>
2018-07-23 10:39:21 +0200

    docs: user anonymization & robot/machine flagging

--- invenio_stats/processors.py
+++ invenio_stats/processors.py
@@ -25,7 +25,27 @@ from .utils import get_anonymization_salt, get_geoip, obj_or_import_string
 
 
 def anonymize_user(doc):
-    """Preprocess an event by anonymizing user information."""
+    """Preprocess an event by anonymizing user information.
+
+    The anonymization is done by removing fields that can uniquely identify a
+    user, such as the user's ID, session ID, IP address and User Agent, and
+    hashing them to produce a ``visitor_id`` and ``unique_session_id``. To
+    further secure the method, a randomly generated 32-byte salt is used, that
+    expires after 24 hours and is discarded. The salt values are stored in
+    Redis (or whichever backend Invenio-Cache uses). The ``unique_session_id``
+    is calculated in the same way as the ``visitor_id``, with the only
+    difference that it also takes into account the hour of the event . All of
+    these rules effectively mean that a user can have a unique ``visitor_id``
+    for each day and unique ``unique_session_id`` for each hour of a day.
+
+    This session ID generation process was designed according to the `Project
+    COUNTER Code of Practice <https://www.projectcounter.org/code-of-
+    practice-sections/general-information/>`_.
+
+    In addition to that the country of the user is extracted from the IP
+    address as a ISO 3166-1 alpha-2 two-letter country code (e.g. "CH" for
+    Switzerland).
+    """
     ip = doc.pop('ip_address', None)
     if ip:
         doc.update({'country': get_geoip(ip)})
@@ -76,13 +96,30 @@ def anonymize_user(doc):
 
 
 def flag_robots(doc):
-    """Flag events which are created by robots."""
+    """Flag events which are created by robots.
+
+    The list of robots is defined by the `COUNTER-robots Python package
+    <https://github.com/inveniosoftware/counter-robots>`_ , which follows the
+    `list defined by Project COUNTER
+    <https://www.projectcounter.org/appendices/850-2/>`_ that was later split
+    into robots and machines by `the Make Data Count project
+    <https://github.com/CDLUC3/Make-Data-Count/tree/master/user-agents>`_.
+    """
     doc['is_robot'] = 'user_agent' in doc and is_robot(doc['user_agent'])
     return doc
 
 
 def flag_machines(doc):
-    """Flag events which are created by machines."""
+    """Flag events which are created by machines.
+
+    The list of machines is defined by the `COUNTER-robots Python package
+    <https://github.com/inveniosoftware/counter-robots>`_ , which follows the
+    `list defined by Project COUNTER
+    <https://www.projectcounter.org/appendices/850-2/>`_ that was later split
+    into robots and machines by `the Make Data Count project
+    <https://github.com/CDLUC3/Make-Data-Count/tree/master/user-agents>`_.
+
+    """
     doc['is_machine'] = 'user_agent' in doc and is_machine(doc['user_agent'])
     return doc
 
