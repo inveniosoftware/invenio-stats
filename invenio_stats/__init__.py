363218453c7687ae0098773180a79dbd9e697cd9
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2025-04-30 00:15:52 +0200

    📦 release: v4.4.0

--- invenio_stats/__init__.py
+++ invenio_stats/__init__.py
@@ -4,6 +4,7 @@
 # Copyright (C) 2017-2025 CERN.
 # Copyright (C) 2022-2023 TU Wien.
 # Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -441,7 +442,7 @@ Invenio-Stats provides some default statistics which can be found in
 from .ext import InvenioStats
 from .proxies import current_stats
 
-__version__ = "4.3.0"
+__version__ = "4.4.0"
 
 __all__ = (
     "__version__",
